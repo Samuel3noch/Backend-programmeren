781ecd5e028041aad79a765524a2908e60a570b5
Samuel <Semnfumuntoto@hotmail.com>
2025-08-27 14:04:41 +0200

    Add PHP lab and knowledge test files, update README
    Added lab and knowledge test files for PHP sections 1.1 and 1.2, including instructions and sample code. Improved answers in the knowledge test 1.1 file and updated README to reflect new sections and files.

---
+++ 1. PHP/1.2 The development environment/first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>testing</title>
+</head>
+<?php 
+    echo phpversion();
+    phpinfo();
+?>
+<body> 
+</body>
+</html>
